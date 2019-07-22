61e4099cdec6ac2eff35ddc851d1defc71f8cedd
Feliphe Marinho <feliphemarinho18@gmail.com>
2019-07-22 17:55:36 -0300

    Consulta simples com query methods

--- src/main/java/com/feliphe/workshopmongo/services/PostService.java
+++ src/main/java/com/feliphe/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.feliphe.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,4 +23,7 @@ public class PostService {
 		return 	post.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado."));
 	}
 	
+	public List<Post> findByTitle(String text) {
+		return postRepository.findByTitleContainingIgnoreCase(text);
+	}
 }
